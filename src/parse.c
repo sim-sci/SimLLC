88024dad66d00f1073f9329b1ba9e60127df2a81
CompileMyLife <ibrah5@pdx.edu>
2022-11-24 18:00:39 -0800

    modified the way to parse file into possibly being able to take values straight up from file as parsed

--- src/parse.c
+++ src/parse.c
@@ -13,7 +13,7 @@
 
 #include "parse.h"
 
-int parse_file(char *filename){
+void parse_file(char *filename){
     FILE *fp = NULL;
 
     // Read in filename given
@@ -32,16 +32,152 @@ int parse_file(char *filename){
     //    printf("%c %" PRIx64 " \n", type, address);
     //}
 
+    //ln = 0;
+    //// WHILE the file is not EOF and no file error
+    //while (!feof(fp) && !ferror(fp)){
+    //    // Read next line of file and store into data array. Store only MAX_LEN
+    //    // per line. IF there is more lines to read than increment ln
+    //    if (fgets(data[ln], MAX_LEN, fp) != NULL){ 
+    //        ln++;
+    //    }
+    //}
+
     ln = 0;
-    // WHILE the file is not EOF and no file error
-    while (!feof(fp) && !ferror(fp)){
-        // Read next line of file and store into data array. Store only MAX_LEN
-        // per line. IF there is more lines to read than increment ln
-        if (fgets(data[ln], MAX_LEN, fp) != NULL){ 
-            ln++;
-        }
+    while ((fscanf(fp, "%d %s", &n[ln], data[ln])) != EOF){
+        ln++;
+    }
+    fclose(fp);
+
+    for (int i = 0; i <= ln; i++){
+        printf("%d %s", n[i], data[i]);
+    }
+    //for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
+    //    substr(data[i], n[i], 
+    //}
+    //
+}
+
+char *hex2bin(char *hex) {
+
+    // Maximum buffersize
+    static char binary[100];	
+	long int count  = 0;
+	
+	while(hex[count]){
+		
+	    switch(hex[count]){
+            case '0': 
+		        //printf("0000");
+                strcat(binary, "0000");
+		        break;
+
+		    case '1':
+		        //printf("0001");
+                strcat(binary, "0001");
+		        break;
+
+		    case '2':
+		        //printf("0010");
+                strcat(binary, "0010");
+		        break;
+
+		    case '3':
+		        //printf("0011");
+                strcat(binary, "0011");
+		        break;
+		    
+		    case '4':
+		        //printf("0100");
+                strcat(binary, "0100");
+		        break;
+		    
+		    case '5':
+		        //printf("0101");
+                strcat(binary, "0101");
+		        break;
+		    
+		    case '6':
+		        //printf("0110");
+                strcat(binary, "0110");
+		        break;
+		    
+		    case '7':
+		        //printf("0111");
+                strcat(binary, "0111");
+		        break;
+
+		    case '8':
+		        //printf("1000");
+                strcat(binary, "1000");
+		        break;
+		    
+		    case '9':
+		        //printf("1001");
+                strcat(binary, "1001");
+		        break;
+		    
+		    case 'a':
+		    case 'A':
+		        //printf("1010");
+                strcat(binary, "1010");
+		        break;
+		    
+		    case 'b':
+		    case 'B':
+		        //printf("1011");
+                strcat(binary, "1011");
+		        break;
+		    
+		    case 'c':
+		    case 'C':
+		        //printf("1100");
+                strcat(binary, "1100");
+		        break;	
+	
+		    case 'd':
+		    case 'D':
+		        //printf("1101");
+                strcat(binary, "1101");
+		        break;
+
+		    case 'e':
+		    case 'E':
+		        //printf("1110");
+                strcat(binary, "1110");
+		        break;
+
+		    case 'f':
+		    case 'F':
+		        //printf("1111");
+                strcat(binary, "1111");
+		        break;
+
+		    default:
+		        printf("invalid input");
+		        break;
+		}
+	 	count++;
+	}
+	
+    return binary;
+}
+
+void substr(char *str_og, char *substr, int idx, int len){
+    // Check if index is out of range of length of original string
+    if (idx >= strlen(str_og)){
+        fprintf(stderr, "Error: index argument is out of range of original string\n");
+        return;
     }
 
-    fclose(fp);
-    return 0;
+    // Copy from original string at index to len into substring AND check if
+    // the next shifted position of original string to be copied is not the
+    // null character
+    int i = 0;
+    while ((i < len) && (str_og[idx + i] != '\0')){
+        substr[i] = str_og[idx + i];
+        i++;
+    }
+
+    // Add null character to string
+    substr[len] = '\0';
 }
--- src/parse.h
+++ src/parse.h
@@ -16,17 +16,33 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 
 // Limits from taking in data from file
 #define MAX_LINES 100
 #define MAX_LEN 100
 
-// Global for main.c to use
-char data [MAX_LINES][MAX_LEN];
 int ln;
 
+// Global for main.c to use
+char data [MAX_LINES][MAX_LEN];
+
+char * address;
+// The tag, byte select, index and operation n using the index of which element
+// in arrays
+int *tag;
+int *byte_sel;
+int *indx;
+int *n;
+
 // Parse a trace file by given name
-int parse_file(char *file_name);
+void parse_file(char *file_name);
+
+// Convert hex string from tracefile to binary string
+char *hex2bin(char *hex);
+
+// Take original string and create substring from it based on index of original
+// string and length of desired substring
+void substr(char *str_og, char *substr, int idx, int len);
+
 
 #endif
